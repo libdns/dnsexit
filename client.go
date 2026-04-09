ca7c85ab3a8975cb92f0a707a610f315182fecb7
Steve Holmes <gituser@megatno.com>
2026-04-10 00:25:28 +0100

    Debug log apiKey and add test to prove that apiKey is set in the HTTP header

--- client.go
+++ client.go
@@ -143,6 +143,7 @@ func (p *Provider) amendRecords(zone string, records []libdns.Record, action Act
 	if debug {
 		fmt.Println("Request Info:")
 		fmt.Println("Url:", string(updateURL))
+		fmt.Println("Header apikey:", p.APIKey)
 		fmt.Println("Body:", string(reqBody))
 	}
 
